71304f6c8417066fade5d66d5feca625a3112f6e
Filipe Medeiros <filipecmedeiros88@gmail.com>
2023-11-26 16:59:07 -0300

    Part 2

--- 2 - R Programming/week 2/Programming Assignment 1: Air Pollution.R
+++ 2 - R Programming/week 2/Programming Assignment 1: Air Pollution.R
@@ -12,3 +12,17 @@ pollutantmean <- function(directory, pollutant, id=1:332){
 }
 
 
+complete <- function(directory, id=1:332){
+    df <- data.frame(id=integer(), nobs=integer())
+    for (i in id){
+        file_path = paste(directory, "/", sprintf("%03d", i), ".csv",  sep = "")
+        #print(file_path)
+        df_aux = read.csv(file_path)
+        df_aux <- df_aux[complete.cases(df_aux), ]
+        
+        row <- data.frame(id=i, nobs=nrow(df_aux))
+        df <- rbind(df, row)
+    }
+    
+    df
+}
